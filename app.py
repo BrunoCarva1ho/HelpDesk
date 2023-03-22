fcee57866e45d89f6c4e3900b3deefe9ec1336bd
Bruno Carvalho <86502087+BrunoCarva1ho@users.noreply.github.com>
2023-03-22 17:57:13 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 from bd import mysql
 from graf import grafico
-from flask_login import LoginManager, login_user, logout_user, current_user, login_required, UserMixin
 
 app = Flask(__name__)
+app.config['SECRET_KEY']=['chave']
 
 
+#Rota e funcao da página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,39 +19,59 @@ def entrar():
         email = request.form['email']
         senha = request.form['senha']
         
+        if email == '' or senha == '':
+            flash("Preencha todos os campos!")
+            return redirect('entrar.html')
+        
         with mysql.cursor() as cur:
             #Testa se o login é de um administrador
             cur.execute('SELECT idAdmin FROM admin WHERE email=%s AND senha=%s', (email,senha) )
             id = cur.fetchall()
-
-            #Se o id não foi preenchido
+            
+            #Testa se o id não foi preenchido
             if id == ():            
                 #Procura os dados de email e senha no banco aluno
                 cur.execute('SELECT idAluno FROM aluno WHERE email=%s AND senha=%s', (email,senha) )
                 id = cur.fetchall()
-  
-            #Se o id não foi preenchido
+       
+            #Testa se o id ainda não foi preenchido
             if id == ():            
                 #Procura os dados de email e senha no banco professor
                 cur.execute('SELECT idProfessor FROM professor WHERE email=%s AND senha=%s', (email,senha) )
                 id = cur.fetchall()
-            
-            #Se o id foi preenchido
+
+
+            #Testa se o id foi preenchido
             if id != ():    
                 with mysql.cursor() as cur:
                     id = str(id)
-                    id = id[2:3]
+                    id = formata_id(id)
                     
                     cur.execute('SELECT * FROM relato')
                     geral = cur.fetchall()
                     
                     id = int(id)
                     return render_template('relatos.html',geral=geral,id=id)
-
-    #Usuário e senha não encontrados retorna para a mesma pagina
+            
+            #Se o id nao foi preenchido, usuário e senha não encontrados, redireciona para a mesma página
+            else:
+                flash("Email ou senha inválidos!")
+                return redirect('entrar.html')
+    
     return render_template('entrar.html')
 
 
+#Funçao que vai recortar a string id e formatar para o tipo inteiro
+def formata_id(id):
+    id = id.replace("(","")
+    id = id.replace(")","")
+    id = id.replace(",","")
+    id = int(id)
+    
+    return id
+
+
+#Rota e funcao para cadastro de alunos
 @app.route('/cadastraraluno.html', methods =['get','POST'])
 def cadastrarAluno():
     
@@ -62,23 +83,35 @@ def cadastrarAluno():
         idade = request.form['idade']
         senha = request.form['senha']
         
+        if nome == '' or curso == '' or email == '' or idade == '' or senha == '':
+            flash("Preencha todos os campos!")
+            return redirect('cadastraraluno.html')
+        
         #Com o cursor do mysql, executa os comandos de consulta e inserção de dados
         with mysql.cursor() as cur:
             cur.execute('SELECT MAX(idAluno) FROM aluno')   #Consulta para obter o último idAluno cadastrado
             id = cur.fetchmany()
             id = str(id)
-            id = id[2:3]
-            id = int(id)
+            id = formata_id(id)
             id = id+2   #Faz com que o idAluno seja sempre ímpar
             
-            cur.execute('INSERT INTO aluno(nome,curso,email,idade,senha,idAluno) VALUES (%s,%s,%s,%s,%s,%s)',(nome,curso,email,idade,senha,id ))
-            cur.connection.commit()
+            cur.execute('SELECT * FROM aluno WHERE email=%s',email)
+            teste = cur.fetchall()
+            if teste:
+                flash("Este email já foi utilizado!")
+                return redirect('cadastraraluno.html')
                 
-            return render_template('entrar.html')
+            else:
+                cur.execute('INSERT INTO aluno(nome,curso,email,idade,senha,idAluno) VALUES (%s,%s,%s,%s,%s,%s)',(nome,curso,email,idade,senha,id ))
+                cur.connection.commit()
+                
+                return render_template('entrar.html')
+    
     
     return render_template('cadastraraluno.html')
 
 
+#Rota e funcao para cadastro de professores
 @app.route('/cadastrarprofessor.html', methods =['get','POST'])
 def cadastrarProfessor():
     
@@ -89,18 +122,32 @@ def cadastrarProfessor():
         idade = request.form['idade']
         senha = request.form['senha']
         
+        if nome == '' or departamento == '' or email == '' or idade == '' or senha == '':
+            flash("Preencha todos os campos!")
+            return redirect('cadastrarprofessor.html')
+        
         with mysql.cursor() as cur:
             cur.execute('SELECT MAX(idProfessor) FROM professor')
             id = cur.fetchmany()
+            print(id)
             id = str(id)
-            id = id[2:3]
-            id = int(id)
+            id = formata_id(id)
             id = id+2
             
-            cur.execute('INSERT INTO professor(nome,departamento,email,idade,senha,idProfessor) VALUES (%s,%s,%s,%s,%s,%s)',(nome,departamento,email,idade,senha,id))
-            cur.connection.commit()
+            cur.execute('SELECT * FROM professor WHERE email=%s',email)
+            teste = cur.fetchall()
+            
+            if teste:
+                flash("Este email já foi utilizado!")
+                return redirect('cadastrarprofessor.html')
+            
+            else:
+                cur.execute('INSERT INTO professor(nome,departamento,email,idade,senha,idProfessor) VALUES (%s,%s,%s,%s,%s,%s)',(nome,departamento,email,idade,senha,id))
+                cur.connection.commit()
                 
-            return render_template('entrar.html')
+                return render_template('entrar.html')
+                
+
     
     return render_template('cadastrarprofessor.html')
 
@@ -184,7 +231,7 @@ def filtro(id):
     
     return render_template("relatos.html",geral=geral,id=id)
 
-
+#rota e funcao de excluir do admin
 @app.route('/excluir/<idRelato>/<int:id>')
 def excluir(idRelato,id):
     
@@ -199,6 +246,7 @@ def excluir(idRelato,id):
     return render_template("relatos.html", geral=geral, id=id)
 
 
+#rota e funcao de excluir do aluno 
 @app.route('/excluir_historico/<idRelato>/<int:id>')
 def excluir_historico(idRelato,id):
     
@@ -206,11 +254,12 @@ def excluir_historico(idRelato,id):
         cur.execute("delete FROM relato WHERE idRelato=%s",idRelato)
         cur.connection.commit()
         
-        cur.execute('select * from relato')
+        cur.execute('select * from relato where idAutor=%s',id)
         geral = cur.fetchall()
     
     
     return render_template("historico.html", geral=geral, id=id)
+
 
 
 if __name__ == '__main__':
